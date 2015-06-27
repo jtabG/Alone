af9f79b6a9bd7a2c72ab820569c9b0c27fc98bfd
Zornag <kevintom08@gmail.com>
2015-06-27 19:36:52 -0400

    Linked up Start Screen/Start Menu/How to Play Screen
    Imported scripts/UI assets

---
+++ MazeJammer/Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Text counterText;
+	
+	private float seconds, minutes;
+
+	void Start() 
+	{
+		counterText = GetComponent<Text> () as Text;
+	}
+
+	void Update() {
+
+		minutes = (int)(Time.timeSinceLevelLoad / 60f); //Divide the guiTime by sixty to get the minutes.
+		seconds = (int)(Time.timeSinceLevelLoad % 60f);//Use the euclidean division for the seconds.
+		counterText.text = minutes.ToString ("00") + ":" + seconds.ToString ("00");
+	}
+}
