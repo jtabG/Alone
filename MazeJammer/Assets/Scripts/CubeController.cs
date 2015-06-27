e49ba820c79bcb3b9c4e4e6b158fe1693d4c312b
jtabG <jtabGazzola@gmail.com>
2015-06-26 22:05:05 -0400

    Base cube created
    prefab for base cube and initial components created

---
+++ MazeJammer/Assets/Scripts/CubeController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+    [SerializeField]
+    private TrapType m_TrapType = TrapType.NONE;
+
+    private TrapState m_TrapState = TrapState.IDLE;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+
+}
---
+++ MazeJammer/Assets/Scripts/Enums.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum TrapType
+{
+    NONE,
+    PIT,
+    FIRE,
+    WURM
+}
+
+public enum TrapState
+{
+    DISABLED,
+    IDLE,
+    TRIGGERED,
+    ACTIVE,
+    RESETTING
+}
