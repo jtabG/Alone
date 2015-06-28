551f5101efd6be917acae455cb4c593232f2d02d
jtabG <jtabGazzola@gmail.com>
2015-06-27 20:25:41 -0400

    Completed flotater movement
    • floater rotates properly around player

--- MazeJammer/Assets/Scripts/AIFloaterBehaviour.cs
+++ MazeJammer/Assets/Scripts/AIFloaterBehaviour.cs
@@ -9,21 +9,49 @@ public class AIFloaterBehaviour : MonoBehaviour, IAIBehaviour
     private float m_MoveSpeed = 2.0f;
 
     [SerializeField]
+    private float m_yOffset = 0.5f;
+
+    [SerializeField]
     private float m_DistanceFromPlayer = 3.0f;
 
+    private float m_RotationAmount = 0.0f;
 	// Use this for initialization
 	void Start ()
     {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
     {
-        Vector3 targetPosition = transform.position;
-        targetPosition.x += m_DistanceFromPlayer * Mathf.Sin(Time.deltaTime);
-        targetPosition.z += m_DistanceFromPlayer * Mathf.Cos(Time.deltaTime);
+        UpdatePostionRotation();
+        CheckForTraps();
 	}
+
+    private void UpdatePostionRotation()
+    {
+        Vector3 targetPosition = m_Player.transform.position;
+        targetPosition.y += m_yOffset + (Mathf.Sin(Time.time));
+
+        m_RotationAmount += Time.deltaTime + m_MoveSpeed;
+
+        if (m_RotationAmount > 360.0f) { m_RotationAmount = 0.0f; }
+
+        float rotationRad = m_RotationAmount * Mathf.Deg2Rad;
+
+        targetPosition.x += m_DistanceFromPlayer * Mathf.Sin(rotationRad);
+        targetPosition.z += m_DistanceFromPlayer * Mathf.Cos(rotationRad);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * m_MoveSpeed);
+
+        transform.Rotate(transform.right, (Mathf.Cos(Time.time)));
+    }
+
+    private void CheckForTraps()
+    {
+
+    }
+
 
     public void AssignPlayerReference(GameObject aPlayer)
     {
@@ -33,6 +61,7 @@ public class AIFloaterBehaviour : MonoBehaviour, IAIBehaviour
     public void Reset()
     {
         transform.position = m_Player.transform.position + (m_Player.transform.right * m_DistanceFromPlayer);
+        transform.position += new Vector3(0.0f, m_yOffset, 0.0f);
     }
 
     public void UpdateState()
