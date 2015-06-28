60ed8bf56b3b791390521ca45ed6c7e6c375fa07
Connie Fu <fu000032@algonquinlive.com>
2015-06-27 21:10:50 -0400

    ummmmm

--- MazeJammer/Assets/Scripts/Traps/SpikeTrapBehaviour.cs
+++ MazeJammer/Assets/Scripts/Traps/SpikeTrapBehaviour.cs
@@ -14,22 +14,39 @@ public class SpikeTrapBehaviour : MonoBehaviour, ITrap
         m_SpikeAnimator = GetComponentInChildren<Animator>();
         if (m_SpikeAnimator == null)
         {
-            Debug.LogWarning("
+            Debug.LogWarning("Spike Animator is NULL!");
         }
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	    
+        // this could b more efficient but ... w/e
+	    switch(m_State)
+        {
+            case TrapState.ACTIVE:
+            case TrapState.DISABLED:
+            case TrapState.IDLE:
+            case TrapState.RESETTING:
+            case TrapState.TRIGGERED:
+            default:
+                Debug.LogError("Default trapstate???");
+                break;
+        }
 	}
 
     void updateAnimations()
     {
-
+        //m_SpikeAnimator
     }
 
     public void OnTriggerEnter()
+    {
+     	
+    }
+
+
+    public void TrapDetected()
     {
      	
     }
