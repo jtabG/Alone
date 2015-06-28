6736073742e7c7fbada4195c5bbe8e70816088bb
Connie Fu <fu000032@algonquinlive.com>
2015-06-28 18:22:37 -0400

    yea

--- MazeJammer/Assets/Scripts/GameUIText.cs
+++ MazeJammer/Assets/Scripts/GameUIText.cs
@@ -16,19 +16,41 @@ public class GameUIText : MonoBehaviour
     [SerializeField]
     private GameStats.levelStats m_LevelStats;
 
+    private int m_Minutes;
+    private int m_Seconds;
+
 	void Start () 
     {
         m_GameManager = GameObject.Find("GameManager").GetComponent<GameController>();
         m_LevelStats = m_GameManager.getLevel();
+
+        if (m_GameManager == null ||
+            m_LevelStats == null ||
+            m_DeathCounterText == null ||
+            m_LevelText == null ||
+            m_TimerText == null)
+        {
+            Debug.LogWarning("Something is NULL");
+        }       
 	}
 
 	void Update () 
     {
-
+        m_LevelText.text = "LEVEL " + ((int)m_GameManager.m_CurrentLevel).ToString("00");
+        updateTimer();
+        updateDeathCounter();
 	}
 
     void updateTimer()
     {
+        m_Minutes = (int)(Time.timeSinceLevelLoad / 60.0f);
+        m_Seconds = (int)(Time.timeSinceLevelLoad % 60.0f);
 
+        m_TimerText.text = m_Minutes.ToString("00") + ":" + m_Seconds.ToString("00");
+    }
+
+    void updateDeathCounter()
+    {
+        m_DeathCounterText.text = m_LevelStats.CurrentNumberDeaths.ToString("00");
     }
 }
